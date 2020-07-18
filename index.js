bba471f8a0d62f8ea66511c532bf7deb473b414f
Brandon <brandon.shihabi@gmail.com>
2020-07-17 21:43:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const https = require("https");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+const schedule = require("node-schedule");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content === "!arcade") {
+    getArcadeModes(message);
+  }
+});
+
+function getArcadeModes(message) {
+  https
+    .get("https://overwatcharcade.today/api/overwatch/today", (resp) => {
+      let data = "";
+
+      // A chunk of data has been recieved.
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        //   console.log("got data " + data)
+        let modes = [];
+        data = JSON.parse(data).modes;
+        Object.keys(data).forEach((value) => {
+          modes.push(data[value].name);
+        });
+        let formattedModes = modes.map((e) => " " + e);
+        message.reply("Current modes for today are:" + formattedModes);
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
+
+function isCTFBlitzIn() {
+  https
+    .get("https://overwatcharcade.today/api/overwatch/today", (resp) => {
+      let data = "";
+
+      // A chunk of data has been recieved.
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        //   console.log("got data " + data)
+        let ids = [];
+        let currentModes = JSON.parse(data).modes;
+        Object.keys(currentModes).forEach((value) => {
+          ids.push(currentModes[value].id);
+        });
+        console.log(ids);
+        https.get(
+          "https://overwatcharcade.today/api/overwatch/arcademodes",
+          (resp) => {
+            let chunks = "";
+            resp.on("data", (chunk) => {
+              chunks += chunk;
+            });
+            resp.on("end", () => {
+              let ctfBlitzMode;
+              chunks = JSON.parse(chunks);
+              Object.keys(chunks).forEach((value) => {
+                if (chunks[value].name === "CTF Blitz") {
+                  ctfBlitzMode = chunks[value].id;
+                }
+              });
+              if (ids.includes(ctfBlitzMode)) {
+                client.channels.cache
+                  .get("688181231707488266")
+                  .send(
+                    "<@&689571428177281049> CTF Blitz is in! Time to troll"
+                  );
+              }
+            });
+          }
+        );
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
+
+var rule = new schedule.RecurrenceRule();
+rule.hour = 17;
+rule.minute = 10;
+
+var j = schedule.scheduleJob(rule, () => {
+  console.log("checking for ctf blitz");
+  isCTFBlitzIn();
+});
+
+client.login("NzMzODgwNTI4NzAwMzA5NTQ1.XxJllw.2nrZCsl-qhzvX90bmUUTc2vOK-E");
